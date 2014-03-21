4a74bba911bdded8228a9bbf8a3bbe5488c6120c
Khalid Abuhakmeh <khalid@aquabirdconsulting.com>
2014-03-21 11:40:06 -0400

    added concept of Hydrators.

---
+++ source/PerfMatters.Flush/Hydrators/IHydrator.cs
@@ -0,0 +1,46 @@
+﻿using System.Web.Mvc;
+
+namespace PerfMatters.Flush.Hydrators
+{
+    public interface IHydrator
+    {
+        void Hydrate(ControllerBase controller);
+    }
+
+    public class TitleHydrator : IHydrator
+    {
+        private readonly string _title;
+
+        public TitleHydrator(string title)
+        {
+            _title = title;
+        }
+
+        public void Hydrate(ControllerBase controller)
+        {
+            if (string.IsNullOrEmpty(_title))
+                return;
+
+            controller.ViewBag.Title = _title;
+        }
+    }
+
+    public class ModelHydrator : IHydrator
+    {
+        private readonly object _model;
+
+        public ModelHydrator(object model)
+        {
+            _model = model;
+        }
+
+        public void Hydrate(ControllerBase controller)
+        {
+            if (_model == null)
+                return;
+
+            controller.ViewData.Model = _model;
+        }
+    }
+
+}
